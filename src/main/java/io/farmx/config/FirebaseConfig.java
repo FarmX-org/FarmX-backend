5635af71af83da8d6afb66819ae69b27f245902d
talamyaseen <talamyaseen>
2025-06-21 11:50:24 +0300

    notification deployed

--- src/main/java/io/farmx/config/FirebaseConfig.java
+++ src/main/java/io/farmx/config/FirebaseConfig.java
@@ -1,33 +1,42 @@
 package io.farmx.config;
 
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import java.io.*;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CREDENTIALS:}")
+    private String firebaseEnvCredentials;
+
     @PostConstruct
     public void initializeFirebase() throws IOException {
-        		InputStream serviceAccount = new ClassPathResource("firebase/test.json").getInputStream();
-        		FirebaseOptions options = new FirebaseOptions.Builder()
-        		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        		  .build();
+        InputStream serviceAccount;
 
+        if (!firebaseEnvCredentials.isEmpty()) {
+            // running on Render, decode string
+            byte[] decoded = Base64.getDecoder().decode(firebaseEnvCredentials);
+            serviceAccount = new ByteArrayInputStream(decoded);
+            System.out.println("🔐 Firebase loaded from ENV variable");
+        } else {
+            // running locally, use JSON file
+            serviceAccount = new FileInputStream("src/main/resources/firebase/serviceAccountKey.json");
+            System.out.println("🧪 Firebase loaded from local file");
+        }
 
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
     }
 }
-
